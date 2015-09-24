08185eb5569b281310bfd245e60f2f8723953af1
juanmanuel-calderon <juanmanuelcalderon812@gmail.com>
2015-09-24 03:10:26 +0200

    Added main.lua file.

---
+++ main.lua
@@ -0,0 +1,23 @@
+--[[
+	Main file of the script.
+	Reads the input file, calculates a solution and exports it.
+--]]
+
+local Utility = require("Utility")
+local PointStructure = require("PointStructure")
+local Algorithm = require("Algorithm")
+
+if(Utility.tlength(arg) ~= 2) then
+	print("Usage = inputfile outputfile")
+else
+	INPUT_FILE = arg[1]
+	OUTPUT_FILE = arg[2]
+
+	local file = Utility.readFile(INPUT_FILE)
+
+	local points = PointStructure.create(file)
+	local start = Algorithm.TSP_Solution(points)
+	PointStructure.export(points, start, OUTPUT_FILE)
+end
+
+print("Done.")
